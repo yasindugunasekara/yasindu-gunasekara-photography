d56ff72bbc6f0bdcc148cf014dc176fcbea9dc01
Yasindu <ramrasu2017@gmail.com>
2025-09-11 02:49:29 +0530

    change portfolio buttons not selected color to white

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -55,7 +55,7 @@ const Portfolio: React.FC = () => {
               className={`px-4 py-2 rounded-full font-medium transition-all duration-300 ${
               selectedCategory === category.id
                 ? 'bg-amber-500 text-white shadow-lg'
-                : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
+                : 'bg-white-100 text-gray-700 hover:bg-gray-200'
               }`}
             >
               {category.label}
