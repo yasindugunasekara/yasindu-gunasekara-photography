868eee0743cbbaafa7c560d9f1a269b0b7358195
Yasindu <ramrasu2017@gmail.com>
2025-09-11 02:50:26 +0530

    remove service cart icones

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -44,7 +44,6 @@ const Services: React.FC = () => {
                 <div className="absolute inset-0 bg-black bg-opacity-50 group-hover:bg-opacity-70 transition-all duration-300"></div>
                 {/* Overlayed Content */}
                 <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-6">
-                  <div className="mb-4 text-4xl">{service.icon}</div>
                   <h3 className="text-2xl font-bold text-white mb-2 drop-shadow-lg">
                     {service.title}
                   </h3>
