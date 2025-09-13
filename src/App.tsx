de6e53c3b2b3039f8857314455abc3e7305034ec
Yasindu <ramrasu2017@gmail.com>
2025-09-13 13:13:01 +0530

    add react route for gallrty page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
@@ -7,18 +7,32 @@ import Portfolio from './components/Portfolio';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import GalleryPage from './pages/GalleryPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Hero />
-        <Services />
-        <Portfolio />
-        <About />
-        <Contact />
-        <Footer />
+        <Routes>
+          {/* Home Page */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Services />
+                <Portfolio />
+                <About />
+                <Contact />
+                <Footer />
+              </>
+            }
+          />
+          
+          {/* Gallery Page */}
+          <Route path="/gallery/:id" element={<GalleryPage />} />
+        </Routes>
       </div>
     </Router>
   );
