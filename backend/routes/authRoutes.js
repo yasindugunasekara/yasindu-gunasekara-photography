5efddea63b1917193396d7475687f1cdd8e3586e
Yasindu <ramrasu2017@gmail\.com>
2026-09-22 09:04:29 +0530

    change logging route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { login, changePassword, refresh, logout } = require("../controllers/authController");
 const authenticateToken = require("../middleware/auth");
 
-router.post("/login", login);
+router.post("/", login);
 router.post("/refresh", refresh);
 router.post("/logout", logout);
 router.put("/admin/password", authenticateToken, changePassword);
